8e00a725d9b38cdf64da517b86bb45e58221571d
Olesia Baidala <99407184+Olesia32@users.noreply.github.com>
2026-05-03 15:34:24 +0300

    Update Program.cs

--- backend/SkillSwap.Api/Program.cs
+++ backend/SkillSwap.Api/Program.cs
@@ -62,7 +62,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp", policy =>
     {
         policy
-            .WithOrigins("http://localhost:4200", "https://skill-swap-project-lipk.onrender.com")
+            .WithOrigins("http://localhost:4200", "https://skillswap-frontend-rzb6.onrender.com")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
